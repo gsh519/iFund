43e63ed00ca2c92164b338935edc2b608f705f01
gsh519 <gsh05196yu@yahoo.co.jp>
2022-09-03 11:13:16 +0900

    デザイン　完成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,9 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
     <style>
         body {
@@ -18,7 +21,160 @@
 </head>
 
 <body>
-    <h1>iFund</h1>
+    <header>
+        <h1>iFund</h1>
+    </header>
+    <div class="wrapper">
+        <div class="swipe-wrapper">
+            <ul class="swipe-list">
+                <li>8月</li>
+                <li class="active">9月</li>
+                <li>10月</li>
+            </ul>
+        </div>
+        <!-- 残金額 -->
+        <div class="amount-wrapper">
+            <div class="amount-area">
+                <p>残金額</p>
+                <h2 class="amount">¥125,329</h2>
+            </div>
+        </div>
+        <!-- リスト -->
+        <div class="list-wrapper">
+            <div class="list-header">
+                <p>
+                    <i class="fas fa-trash-alt"></i>
+                    削除
+                </p>
+            </div>
+            <ul class="list">
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥1,000</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥1,000</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥1,000</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥35,000</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥5,000</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥520</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥520</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥520</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥520</p>
+                    </li>
+                </div>
+                <div class="list-item">
+                    <p class="list-date">2022/09/03</p>
+                    <li class="list-flex">
+                        <div class="list-checkbox">
+                            <label for="chckbox">
+                                <input id="chckbox" type="checkbox">
+                            </label>
+                        </div>
+                        <p class="list-text">献立アプリ</p>
+                        <p class="list-money">¥520</p>
+                    </li>
+                </div>
+            </ul>
+        </div>
+
+        <div class="plus-btn">
+            <i class="fas fa-plus"></i>
+        </div>
+    </div>
 </body>
 
 </html>
